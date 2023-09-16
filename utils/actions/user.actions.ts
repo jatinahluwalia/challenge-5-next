93c6ccff6c2ed46e816067fe6b8ce5fe20eb70c6
jitu2404 <103820854+jitu2404@users.noreply.github.com>
2023-09-17 02:29:34 +0530

    reduce user model

--- utils/actions/user.actions.ts
+++ utils/actions/user.actions.ts
@@ -4,20 +4,20 @@ import User from "../models/user.model";
 import connectDB from "../mongoose";
 
 export const fetchUser = async (id: string) => {
+  await connectDB();
   return await User.findOne({ id });
 };
 
 export const addUser = async ({
-  id,
-  name,
-  email,
-  image,
-}: {
+  id, // name,
+} // email,
+// image,
+: {
   id: string;
   name?: string;
   email?: string;
   image?: string;
 }) => {
   await connectDB();
-  return await User.create({ id, name, email, image });
+  return await User.create({ id /* name, email, image */ });
 };
