1567b9d2f4bf88a42544e2ccfec2597e28e5e135
jitu2404 <103820854+jitu2404@users.noreply.github.com>
2023-09-17 02:26:55 +0530

    no longer required

--- utils/models/user.model.ts
+++ utils/models/user.model.ts
@@ -2,8 +2,8 @@ import mongoose from "mongoose";
 
 const UserSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true },
-    email: { type: String, required: true },
+    name: { type: String },
+    email: { type: String },
     image: { type: String },
     id: String,
     comments: [
